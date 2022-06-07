b40e06cf8fddc3d2853159a1e96c891f24073ff1
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2022-06-07 19:46:20 -0300

    Creating server

--- src/api.js
+++ src/api.js
@@ -1,1 +1,26 @@
-console.info('🚀 Your API Running right here!');
+const http = require('http');
+
+const routes = (route) => {
+	switch (route) {
+		default: return (request, response) => {
+			response.write('Hello World!')
+			return response.end();
+		}
+	}
+}
+
+const handler = function (request, response) {
+	const { url, method } = request
+	const routeKey = `${url}:${method.toLowerCase()}`
+	const chosen = routes(routeKey)
+
+	response.writeHead(200, {
+		'Content-Type': 'text/html'
+	})
+	return chosen(request, response)
+}
+
+const app = http.createServer(handler)
+	.listen(3000, () => console.log('runing on port 3000'));
+
+module.exports = app
